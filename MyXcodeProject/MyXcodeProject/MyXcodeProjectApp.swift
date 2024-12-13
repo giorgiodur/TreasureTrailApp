3c21a38b840a9de687ff1ea78bf3007e711e9a3b
Giorgio <gorgo.durante@gmail.com>
2024-12-13 11:24:19 +0100

    Initial project setup

---
+++ MyXcodeProject/MyXcodeProject/MyXcodeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyXcodeProjectApp.swift
+//  MyXcodeProject
+//
+//  Created by Giorgio Durante on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MyXcodeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
