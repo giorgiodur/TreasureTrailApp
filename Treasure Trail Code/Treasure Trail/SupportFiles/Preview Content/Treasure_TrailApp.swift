d4eae30de89da0277178357879779d0df1f9ebc7
Giorgio <gorgo.durante@gmail.com>
2024-12-18 10:19:29 +0100

    new upload

---
+++ Treasure Trail Code/Treasure Trail/SupportFiles/Preview Content/Treasure_TrailApp.swift
@@ -0,0 +1,17 @@
+//
+//  Treasure_TrailApp.swift
+//  Treasure Trail
+//
+//  Created by Giorgio Durante on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Treasure_TrailApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomePage()
+        }
+    }
+}
